7d634917ada5718785aef387d02a5906269732bb
JasonLunsford <jl630043@gmail.com>
2015-08-28 18:05:31 -0400

    step 6 complete

--- app.js
+++ app.js
@@ -61,7 +61,20 @@ if (Meteor.isClient) {
 		'$meteor',
 		function($scope, $stateParams, $meteor) {
 
-			this.party = $meteor.object(Parties, $stateParams.partyId);
+			// adding false as 3rd parameter prevents autosaving object on every change
+			this.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+			this.save = function() {
+				this.party.save().then(function(numberOfDocs) {
+					console.log('doc '+numberOfDocs+' saved!');
+				}, function(error) {
+					console.log('save error: ', error);
+				});
+			};
+
+			this.reset = function() {
+				this.party.reset();
+			};
 
 		}]);
 }
